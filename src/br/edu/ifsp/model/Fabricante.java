08f1cc76879389d7b049dea68875228bc763a08e
telo47~2016 <telo472014@gmail.com>
2016-06-17 13:20:41 -0300

    Update Fabricante.java
    Feito no github...

--- src/br/edu/ifsp/model/Fabricante.java
+++ src/br/edu/ifsp/model/Fabricante.java
@@ -1,4 +1,45 @@
 package br.edu.ifsp.model;
 
 public class Fabricante implements IModel{
+  int idFabricante;
+  String nome;
+  String endereco;
+  String CNPJ;
+  
+  public int getidFabricante(){
+    return idFabricante;
+  }
+  
+  public void setidFabricante(int idFabricante){
+    this.idFabricante = idFabricante;
+  }
+  
+  public String getnome(){
+    return nome;
+  }
+  
+  public void setnome(String nome){
+    this.nome = nome;
+  }
+  
+  public String getenderco(){
+    return endereco;
+  }
+  
+  public void setendereco(String endereco){
+    this.endereco = endereco;
+  }
+  
+    public String getCNPJ(){
+    return CNPJ;
+  }
+  
+  public void setCNPJ(String CNPJ){
+    this.CNPJ = CNPJ;
+  }
+  
+  
+  
+  
+  
 }
